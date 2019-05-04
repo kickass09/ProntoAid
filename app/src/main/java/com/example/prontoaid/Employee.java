d9ce12d27cdc5526504111cbb4694241fcf6d7e5
TevinJose <tevinjose97@gmail.com>
2019-05-04 12:37:33 +0530

    Working Assignment 1/5/2019

--- app/src/main/java/com/example/prontoaid/Employee.java
+++ app/src/main/java/com/example/prontoaid/Employee.java
@@ -1,7 +1,7 @@
 package com.example.prontoaid;
 
 public class Employee {
-    String Loc;
+
     String User;
     String Phone_Number;
     String Emp_Name;
@@ -20,14 +20,6 @@ public class Employee {
 
     public void setContact(String phone) {     Phone_Number = phone; }
 
-    public String getLocation() {
-        return Loc;
-    }
-
-    public void setLocation(String location) {
-        Loc = location;
-    }
-
     public String getUsername() {
         return User;
     }
@@ -40,9 +32,9 @@ public class Employee {
 
     public void setName(String emp_Name){Emp_Name=emp_Name; }
 
-    public Employee(String location, String username, String phone, String emp_Name, String lats, String longs ) {
+    public Employee(String username, String phone, String emp_Name, String lats, String longs ) {
 
-        Loc = location;
+
         User = username;
         Phone_Number = phone;
         Emp_Name = emp_Name;
